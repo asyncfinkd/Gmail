ba6963a8f35307bf5b0d47e7abbff44dd6fe36bc
asyncfinkd <shamgoingzuck@gmail.com>
2021-05-07 16:56:00 +0400

    :cloud: Application Default Structure

--- src/App.js
+++ src/App.js
@@ -1,22 +1,9 @@
-function App() {
+import React from "react";
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <p>Gmail</p>
+    </>
   );
 }
-
-export default App;
